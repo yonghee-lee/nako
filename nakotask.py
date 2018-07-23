5d8e6bf0812b3937b70b0422fc04239683e5f1bc
yonghee-lee <yongheel@gmail.com>
2018-07-23 18:01:09 +0900

    Update nakotask.py
    add property file reading

--- nakotask.py
+++ nakotask.py
@@ -1,7 +1,16 @@
 from celery import Task
 import decorator
+import nakoutils
+
+properties = nakoutils.readProperties("nakotasks.json")
 
 def task(cls):
-    class Decorator(Task):
-    	pass
-    return type(cls.__name__,(Decorator,)+cls.__bases__,dict(cls.__dict__))
+    
+    taskNames = [x["taskname"] for x in properties if x["module"] == cls.__module__ and x["class"] == cls.__name__]
+    
+    cls.__dict__['name'] = taskNames[0]
+    
+    class TaskDecorator(Task):
+    	pass	
+		
+    return type(cls.__name__,(TaskDecorator,)+cls.__bases__,dict(cls.__dict__))
