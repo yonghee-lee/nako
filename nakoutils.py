b0d851d4138b9f823bf1f9c98ab8f953ab94ee94
yonghee-lee <yongheel@gmail.com>
2018-07-24 18:14:19 +0900

    Update nakoutils.py
    add method for task

--- nakoutils.py
+++ nakoutils.py
@@ -13,9 +13,17 @@ def readProperties(filepath):
 
     return data
 
-def getTaskName(filepath,cls_module,cls_name):
-    taskName = None
-    data = readProperties(filepath)
-    taskName = [x["taskname"] for x in data if x["module"] == cls_module and x["class"] == cls_name]
-    print(len(taskName))
-    return taskName[0]
+def getTaskName(cls,data):
+    
+    taskNames = [x["taskname"] for x in data if x["module"] == cls.__module__ and x["class"] == cls.__name__]
+    
+    return taskNames[0]
+
+def getSparkConfigs(cls,data):
+       
+    sparkAppName = [x["spark_appname"] for x in data if x["module"] == cls.__module__ and x["class"] == cls.__name__ and x["tasktype"] == "spark"]
+    sparkConfigArg1 = [x["spark_config_arg1"] for x in data if x["module"] == cls.__module__ and x["class"] == cls.__name__ and x["tasktype"] == "spark"]
+    sparkConfigArg2 = [x["spark_config_arg2"] for x in data if x["module"] == cls.__module__ and x["class"] == cls.__name__ and x["tasktype"] == "spark"]
+
+
+    return sparkAppName[0],sparkConfigArg1[0],sparkConfigArg2[0]
